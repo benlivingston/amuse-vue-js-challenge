acbc6ed6241454645df9ca32275abb66f03068cf
inwa <ben@hemp.net>
2021-11-21 23:40:03 -0800

    Single product view, closes #6

--- src/router.js
+++ src/router.js
@@ -1,11 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router"
 import Home from "@/views/Home.vue"
+import Product from "@/views/Product.vue"
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/brands/:brand/:product_slug",
+    name: "Product",
+    component: Product,
   }
 ]
 
---
+++ src/views/Product.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrapper">
+    <div class="product">
+      <p>{{ product.name }}</p>
+      <p>{{ product.url }}</p>
+      <p>Cat: {{ product.category }}</p>
+      <p>SubCat: {{ product.sub_category }}</p>
+      <p>{{ product.price }}</p>
+      <p>{{ product.thc_percentage }}</p>
+      <p>{{ product.quantity }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    product() {
+      // match product url to route
+      return this.$store.state.allProducts.filter(p => p.url === this.$route.fullPath)[0]
+    }
+  }
+}
+</script>
+
